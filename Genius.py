c3861384f86497c65a28a214df03d5226b448146
joshuarreid <60335156+joshuarreid@users.noreply.github.com>
2020-09-20 16:12:22 -0400

    Added more functions
    Modified api.py within LyricsGenius

--- Genius.py
+++ Genius.py
@@ -2,6 +2,11 @@ import lyricsgenius
 from Token import client_access_token
 genius = lyricsgenius.Genius(client_access_token)
 
+def getArtistID(artist):
+    artist_id = genius.find_artist_id(artist)
+    return artist_id["id"]
+
+
 def searchSong(title, artist):
     song = genius.search_song(title, artist)
     return song
@@ -17,5 +22,17 @@ def searchWriter(title, artist):
             return writer
         except IndexError:
             return None
+print(getArtistID("Khalid"))
+print("The Song Writer is: " + searchWriter("Khalid", "8TEEN"))
+"""
+search_results = genius.search_genius("Joel Little")["hits"]
+for item in search_results:
+    result = item["result"]
+    print(result)
+    print(result["full_title"])
 
-print("The Song Writer is: " + searchWriter("415", "Anhedonia"))
+"""
+
+
+search_results = genius.search_artist(artist_name="Joel Little", include_features=True)
+print(search_results)
