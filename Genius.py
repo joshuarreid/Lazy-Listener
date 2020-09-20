93f4f497f33d88af4bc63289135faf2afb02dbe5
joshuarreid <60335156+joshuarreid@users.noreply.github.com>
2020-09-20 15:17:44 -0400

    Defining First Functions
    - searchSong()
    - searchWriter()

--- Genius.py
+++ Genius.py
@@ -1,1 +1,21 @@
-import geniuslyrics
+import lyricsgenius
+from Token import client_access_token
+genius = lyricsgenius.Genius(client_access_token)
+
+def searchSong(title, artist):
+    song = genius.search_song(title, artist)
+    return song
+
+
+def searchWriter(title, artist):
+    song = searchSong(title, artist)
+    if song == None:
+        return None
+    else:
+        try:
+            writer = song.writer_artists[0]["name"]
+            return writer
+        except IndexError:
+            return None
+
+print("The Song Writer is: " + searchWriter("415", "Anhedonia"))
