edc4e31ee4783fb1a4b4dbc308f5c2ae1859315c
joshuarreid <60335156+joshuarreid@users.noreply.github.com>
2020-11-26 16:16:26 -0500

    Renaming Project

--- Genius.py
+++ Genius.py
@@ -3,9 +3,8 @@ from Token import client_access_token
 genius = lyricsgenius.Genius(client_access_token)
 
 def getArtistID(artist):
-    artist_id = genius.find_artist_id(artist)
-    return artist_id["id"]
-
+    artistInfo = genius.find_artist_id(artist)
+    return artistInfo
 
 def searchSong(title, artist):
     song = genius.search_song(title, artist)
@@ -22,8 +21,17 @@ def searchWriter(title, artist):
             return writer
         except IndexError:
             return None
-print(getArtistID("Khalid"))
-print("The Song Writer is: " + searchWriter("Khalid", "8TEEN"))
+
+
+
+
+
+KhalidID = getArtistID("Khalid")
+
+
+
+
+#print("The Song Writer is: " + searchWriter("Khalid", "8TEEN"))
 """
 search_results = genius.search_genius("Joel Little")["hits"]
 for item in search_results:
@@ -33,6 +41,6 @@ for item in search_results:
 
 """
 
-
+"""
 search_results = genius.search_artist(artist_name="Joel Little", include_features=True)
-print(search_results)
+print(search_results)"""
